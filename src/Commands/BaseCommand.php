6362e6d710f3af1f00e6e130f23a61e7ea6489b2
Jerry Ablan <jerryablan@dreamfactory.com>
2015-01-06 13:25:27 -0500

    Composer dependencies updated.

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -20,7 +20,6 @@ namespace DreamFactory\Library\Console\Commands;
 
 use DreamFactory\Library\Console\BaseApplication;
 use DreamFactory\Library\Console\Components\Registry;
-use DreamFactory\Library\Console\Enums\AnsiCodes;
 use DreamFactory\Library\Console\Utility\Cursor;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
